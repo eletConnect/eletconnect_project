e12a2d6f3a82e40f3266f163f2aad56e39aca310
Mateus Cotrim <122448632+mateuscotrim@users.noreply.github.com>
2024-08-19 12:49:34 -0300

    Delete back-end/src directory

--- back-end/src/mobile/eletivas/eletiva.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const eletivaController = require("./eletiva.controller");
-
-router.post('/listar', eletivaController.listarEletivas);
-
-module.exports = router;
